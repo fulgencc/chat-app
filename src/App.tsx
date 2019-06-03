27753f9ae2566e3fd3f0114848e757db1feabe7d
chris fulgencio <fulgenc.io>
2019-06-03 07:19:38 -0700

    feat(packages): added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Chat } from './Components/Chat/Chat';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
   return (
