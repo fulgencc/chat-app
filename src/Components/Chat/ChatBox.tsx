1807d6db46b2438343aa656fe4eed31105795084
chris fulgencio <fulgencc@uci.edu>
2019-06-11 17:38:55 -0700

    feat(Chat): scrolling chatbox
    added a scrolling chatbox

--- src/Components/Chat/ChatBox.tsx
+++ src/Components/Chat/ChatBox.tsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 
 // ChatBox component, which receives messages and displays them.
 export function ChatBox(props: { messages: string[] }) {
 
+    useEffect(() => {
+
+        // If a message has been received or sent, scroll to the bottom of the chat.
+        const chatBox = document.getElementById('ChatBox') as HTMLElement;
+        chatBox.scrollTo(0, chatBox.scrollHeight);
+    }, [props.messages])
+
     return (
-        <>
+        <div id='ChatBox' style={{ height: '300px', overflowY: 'scroll' }}>
             {props.messages.map((message, index) => (
                 <p key={index} style={{ wordBreak: "break-all" }}>{message}</p>
             ))}
-        </>
+        </div>
     )
 }
